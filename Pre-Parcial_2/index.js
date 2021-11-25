56238cdb4df1f32afd632b14cb6ead1afa97b71f
EzeMartino <ezequielrmartino@gmail.com>
2021-11-24 21:12:36 -0300

    Malardo

--- Pre-Parcial_2/index.js
+++ Pre-Parcial_2/index.js
@@ -1,3 +1,34 @@
+let indiceSeleccionado = selectorMoneda.selectedIndex
 let monedaExtranjera = document.getElementById("cantidadMoneda")
-let tipoDeMoneda = document.getElementById("selectorMoneda")
 
+function getMonedaExtranjera(){
+    if(monedaExtranjera.value > 0){
+        return monedaExtranjera.value
+    }
+    else{
+        return alert("Ingrese un monto mayor a 0 para efectuar la operación")
+    }
+}
+
+function getSelected(){
+    
+
+    switch(indiceSeleccionado){
+        case 1:
+            return 114.5 //Euro
+        case 2:
+            return 99.75 //Dolar
+    }
+}
+
+function calcularPesos(){
+    let cantExtranjera = getMonedaExtranjera()
+
+    let tipoMoneda = getSelected()
+
+    let pesos = cantExtranjera * tipoMoneda
+
+    return console.log(pesos)
+}
+
+calcular.onclick = calcularPesos;
