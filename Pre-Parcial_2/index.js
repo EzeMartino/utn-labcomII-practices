bec24681737a198d210cba6e3b7280f33cd2768b
EzeMartino <ezequielrmartino@gmail.com>
2021-11-26 19:33:26 -0300

    Cosas

--- Pre-Parcial_2/index.js
+++ Pre-Parcial_2/index.js
@@ -1,34 +1,91 @@
-let indiceSeleccionado = selectorMoneda.selectedIndex
-let monedaExtranjera = document.getElementById("cantidadMoneda")
+let inputPesos = document.getElementById("pesos");
+let monedaExtranjera = document.getElementById("moneda");
 
-function getMonedaExtranjera(){
-    if(monedaExtranjera.value > 0){
-        return monedaExtranjera.value
-    }
-    else{
-        return alert("Ingrese un monto mayor a 0 para efectuar la operación")
-    }
+let boton = document.querySelector("button#calc");
+
+var cotizaciones = {
+    dolares: 105.75,
+    euros: 120.5,
 }
 
-function getSelected(){
+function alertError(inputId){
+    var inputWithErrors = document.getElementById(inputId)
+    inputWithErrors.className = "error"
+}
+
+function clearErrors(inputId) {
+    var inputWithErrors = document.getElementById(inputId);
+    inputWithErrors.className = "";
+}
+
+function validate(){
+    clearErrors(inputPesos.id)
+    if(!Number(inputPesos.value)){
+        alertError(inputPesos.id);
+        return false;
+    }
+    return true;
+}
+
+function displayResult(result) {
+    var tit = document.querySelector("h3");
+    tit.innerText = "$ " + result.toFixed(2);
+}
+
+boton.onclick = function() {
+    if(validate()){
+        let valorPesos = Number(inputPesos.value);
+        let valorMoneda = Number(cotizaciones[monedaExtranjera.value]);
     
-
-    switch(indiceSeleccionado){
-        case 1:
-            return 114.5 //Euro
-        case 2:
-            return 99.75 //Dolar
+        let precioFinal = valorPesos * valorMoneda;
+    
+        displayResult(precioFinal);
+    } else {
+        alert("Flashaste wacho");
     }
+    
 }
 
-function calcularPesos(){
-    let cantExtranjera = getMonedaExtranjera()
+// monedas
+var inputNombre = document.getElementById("nombre");
+var inputValor = document.getElementById("valor");
 
-    let tipoMoneda = getSelected()
+var botoncitNuevaMoneda = document.querySelector("button#nuevaMoneda");
 
-    let pesos = cantExtranjera * tipoMoneda
+function validateNewCoin() {
+  clearErrors(inputNombre.id);
+  clearErrors(inputValor.id);
 
-    return console.log(pesos)
+  var valid = true;
+
+  if (!inputNombre.value) {
+    // nombre no valido
+    alertError(inputNombre.id);
+    valid = false;
+  }
+  if (!Number(inputValor.value)) {
+    // valor no valido
+    alertError(inputValor.id);
+    valid = false;
+  }
+  return valid;
 }
 
-calcular.onclick = calcularPesos;
+botoncitNuevaMoneda.onclick = function () {
+  if (validateNewCoin()) {
+    // get values
+    var nombreMoneda = inputNombre.value;
+    var valorMoneda = Number(inputValor.value);
+
+    // add option to select
+    monedaExtranjera.options.add(new Option(nombreMoneda, nombreMoneda));
+
+    cotizaciones[nombreMoneda] = valorMoneda;
+
+    // reset inputs
+    inputNombre.value = "";
+    inputValor.value = "";
+  } else {
+    alert("algo salio mal");
+  }
+};
